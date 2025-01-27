39a9d02fda9048cb705d691fb7ed49e79532ffee
chuck-dev <edison.mc@gmail.com>
2025-01-26 19:38:02 -0600

    terminado para despliqgue

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+#para poner en produccion
+import os 
+
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +12,18 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+#para produccion
+client_folder = os.path.join(os.getcwd(),"..","client")
+dist_folder = os.path.join(client_folder, "dist")
+
+#archivos estaticos del servidor
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
 
 #api routes
 import routes
