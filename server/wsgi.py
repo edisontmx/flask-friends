300242c89049ed27eb8b1d28e17525184b5b829f
chuck-dev <edison.mc@gmail.com>
2025-01-26 20:11:37 -0600

    correccion de gunicorn

--- server/wsgi.py
+++ server/wsgi.py
@@ -2,3 +2,5 @@ from app import app
 
 if __name__ == "__main__":
     app.run()
+
+#Gunicorn and WSGI (web service gateway interface) are both components  used  in deploying and serving python web aplications, particulary those built with web frameworks like flask and django
